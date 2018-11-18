601a2b97414e3cac2171d636c93811ea2dea6cc3
piotr-s <piotr.slawinski1@gmail.com>
2018-11-18 17:31:25 +0100

    Refactor displayPhotoDetails

--- client/main.js
+++ client/main.js
@@ -12,28 +12,32 @@ function fetchPhotos() {
     return fetch('/photos').then(res => res.json());
 }
 
-function removeFullPhoto() {
-    const $area = document.querySelector('.full');
+function removeFullPhoto(sel) {
+    const $area = document.querySelector(sel);
     if ($area) {
         $area.remove();
     }
 }
 
 function displayPhotoDetails(photo) {
+    removeFullPhoto('.details');
     const $area = document.getElementById('app');
-    let template = document.createElement('div');
-    template = `
+    const $div = document.createElement('div');
+    $div.classList.add('details');
+    const template = `
         <div>
             <p>${photo.author}</p>
             <p>${photo.tags.map((x) => `#${x}`).join(', ')}</p>
             <p>${photo.title}</p>
         </div>
         `;
-    $area.innerHTML += template;
+
+    $div.innerHTML = template;
+    $area.appendChild($div);
 }
 
 function zoomPhoto(photo) {
-    removeFullPhoto();
+    removeFullPhoto('.full');
     const $area = document.getElementById('app');
     const $zoomPhoto = document.createElement('img');
     $zoomPhoto.classList.add('full');
